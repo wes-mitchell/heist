5751543c1d1decfdb6b6f21978d87957df66518e
wes-mitchell <wmdrums@gmail.com>
2022-05-26 14:07:25 -0500

    completes phase one

--- Program.cs
+++ Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Runtime;
 
 namespace Heist
 {
@@ -6,7 +7,24 @@ namespace Heist
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Console.WriteLine("Plan Your Heist!");
+            Console.WriteLine("----------------");
+            Console.Write("What is your team member's name? ");
+            string name = Console.ReadLine();
+            Console.Write("What is the skill level of your member? ");
+            int skill = int.Parse(Console.ReadLine());
+            Console.Write("What is your team member's courage factor (0.0 - 2.0) ");
+            decimal courage = decimal.Parse(Console.ReadLine());
+
+            while (courage < 0 || courage > 2)
+            {
+                Console.Write("What is your team member's courage factor (0.0 - 2.0) ");
+                courage = decimal.Parse(Console.ReadLine());
+            }
+
+            TeamMember member = new TeamMember(name, skill, courage);
+            Console.Write(member);
+       
         }
     }
 }
---
+++ TeamMember.cs
@@ -0,0 +1,30 @@
+using System;
+
+namespace Heist
+{
+  public class TeamMember 
+  {
+    public string Name { get; set; }
+    public int SkillLevel { get; set; }
+    public decimal CourageLevel { get; set; }
+
+    public TeamMember(string name, int skill, decimal courage)
+    {
+      Name = name;
+      SkillLevel = skill;
+      CourageLevel = courage;
+    }
+
+    public override string ToString()
+    {
+      return $"Welcome {Name}, your skill level is {SkillLevel} and your courage level is {CourageLevel}!";
+    }
+
+    // public void ShowDetails()
+    // {
+    //   Console.WriteLine($"Welcome {Name}");
+    //   Console.WriteLine($"Your skill level is {SkillLevel}.");
+    //   Console.WriteLine($"Your courage level is {CourageLevel}");
+    // }
+  } 
+}
