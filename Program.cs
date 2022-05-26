68f04cfaa35311b4c717b66afde6726f545e9a61
wes-mitchell <wmdrums@gmail.com>
2022-05-26 15:27:20 -0500

    completes phase two

--- Program.cs
+++ Program.cs
@@ -1,5 +1,5 @@
 ﻿using System;
-using System.Runtime;
+using System.Collections.Generic;
 
 namespace Heist
 {
@@ -9,6 +9,9 @@ namespace Heist
         {
             Console.WriteLine("Plan Your Heist!");
             Console.WriteLine("----------------");
+
+            List<TeamMember> teamList = new List<TeamMember>();
+
             Console.Write("What is your team member's name? ");
             string name = Console.ReadLine();
             Console.Write("What is the skill level of your member? ");
@@ -22,9 +25,41 @@ namespace Heist
                 courage = decimal.Parse(Console.ReadLine());
             }
 
+            TeamMember firstMember = new TeamMember(name, skill, courage);
+            teamList.Add(firstMember);
+
+            Team newTeam = new Team(teamList);
+
+            newTeam.Description();
+
+            while(true)
+            {
+                Console.Write("What is your team member's name? ");
+                name = Console.ReadLine();
+                if (String.IsNullOrWhiteSpace(name)) 
+                {
+                    break;
+                }
+                else 
+                {
+                    Console.Write("What is the skill level of your member? ");
+                    skill = int.Parse(Console.ReadLine());
+                    Console.Write("What is your team member's courage factor (0.0 - 2.0) ");
+                    courage = decimal.Parse(Console.ReadLine());
+                while (courage < 0 || courage > 2)
+                {
+                    Console.Write("What is your team member's courage factor (0.0 - 2.0) ");
+                    courage = decimal.Parse(Console.ReadLine());
+                }
+                TeamMember thisMember = new TeamMember(name, skill, courage);
+                teamList.Add(thisMember);
+                newTeam.Description();
+                }
+            }
+
+
             TeamMember member = new TeamMember(name, skill, courage);
-            Console.Write(member);
-       
+            newTeam.Description();
         }
     }
 }
